a6f77f1e1d1b43c18086abe56cc2ff78fb5b04ed
Alessandro Ronchi <aleron75@gmail.com>
2017-09-15 13:14:54 +0200

    Update configurable installment options

--- src/app/code/community/Bitbull/Soisy/Model/System/Config/Source/Percentage.php
+++ src/app/code/community/Bitbull/Soisy/Model/System/Config/Source/Percentage.php
@@ -14,13 +14,15 @@ class Bitbull_Soisy_Model_System_Config_Source_Percentage
     {
         return [
             ['label' => 'Off', 'value' => '100'],
-            ['label' => '5%',  'value' => '5'],
             ['label' => '10%', 'value' => '10'],
-            ['label' => '15%', 'value' => '15'],
-            ['label' => '25%', 'value' => '25'],
+            ['label' => '20%', 'value' => '20'],
             ['label' => '30%', 'value' => '30'],
-            ['label' => '45%', 'value' => '45'],
+            ['label' => '40%', 'value' => '40'],
             ['label' => '50%', 'value' => '50'],
+            ['label' => '60%', 'value' => '60'],
+            ['label' => '70%', 'value' => '70'],
+            ['label' => '80%', 'value' => '80'],
+            ['label' => '90%', 'value' => '90'],
         ];
     }
 }
